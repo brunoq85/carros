c9d5a6afeb507a79d376f4dd3210549bebf37f34
brunoq85 <38147160+brunoq85@users.noreply.github.com>
2024-07-17 23:44:30 -0300

    Modificando as páginas em inglês e colocando em português Modificado a configuração  do timezone para 'America/Sao_Paulo' Adicionando alguns carros na base de dados Filtrando por mais de um campo no arquivo admin do app cars Adicionando configurações adicionais no modelo do app cars

--- cars/admin.py
+++ cars/admin.py
@@ -3,7 +3,7 @@ from cars.models import Car
 
 
 class CarAdmin(admin.ModelAdmin):
-    list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
-    search_fields = ('model',)
+    list_display = ('id', 'model', 'brand', 'factory_year', 'model_year', 'value')
+    search_fields = ('model', 'brand',)
 
 admin.site.register(Car, CarAdmin)
